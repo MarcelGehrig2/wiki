451d6cd856ce511150505f3f147382a37f6ba047
Marcel Gehrig <marcel.gehrig@ntb.ch>
2017-01-26 09:36:55 +0100

    commit

---
+++ windows/startSynergy.bat
@@ -0,0 +1,3 @@
+:: http://www.phoenix-int.com/~AnalysisServer/SecureConnections/avoidSVNPasswords.html
+
+plink -i M:\ssh\temp.ppk mgehrig2@146.136.37.213 /home/mgehrig2/wiki/linux/scripts/inUse/startSynergy.sh
