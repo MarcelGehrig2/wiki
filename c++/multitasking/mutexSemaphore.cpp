557bbcbbe9d7f7c73c2bf54115bbed39cc315897
MarcelGehrig <marcel.gehrig@ntb.ch>
2017-02-16 09:00:15 +0100

    commit

---
+++ c++/multitasking/mutexSemaphore.cpp
@@ -0,0 +1,5 @@
+// http://www.geeksforgeeks.org/mutex-vs-semaphore/
+
+// Strictly speaking, a mutex is locking mechanism used to synchronize access to a resource. Only one task (can be a thread or process based on OS abstraction) can acquire the mutex. It means there is ownership associated with mutex, and only the owner can release the lock (mutex).
+
+// Semaphore is signaling mechanism (“I am done, you can carry on” kind of signal). For example, if you are listening songs (assume it as one task) on your mobile and at the same time your friend calls you, an interrupt is triggered upon which an interrupt service routine (ISR) signals the call processing task to wakeup.
--- linux/scripts/inUse/startSynergy.sh
+++ linux/scripts/inUse/startSynergy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pkill -f /usr/bin/synergyc
 
 /usr/bin/synergyc --enable-crypto pcinf053
 /usr/bin/synergyc --enable-crypto pcwinf040
