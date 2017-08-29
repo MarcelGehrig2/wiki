bf28fd58c0d33e369ecf9e53fc6ec86f97017b9b
MarcelGehrig <marcel.gehrig@ntb.ch>
2017-08-29 16:38:56 +0200

    commit

--- linux/scripts/inUse/ROS.sh
+++ linux/scripts/inUse/ROS.sh
@@ -9,9 +9,11 @@ alias sshturtle='ssh kobuki@146.136.59.23'
 source /opt/ros/kinetic/setup.bash
 
 #ROS_MASTER_URI=http://manuel-OptiPlex-790:11311
+ROS_MASTER_URI=http://eeros-inf034-OptiPlex-790:11311
 
 alias useRosIlg=ROS_MASTER_URI=http://manuel-OptiPlex-790:11311
 alias useRosLocal=ROS_MASTER_URI=http://localhost:11311
+alias useRosOpti=ROS_MASTER_URI=http://eeros-inf034-OptiPlex-790:11311
 
 alias motorsim='roslaunch motor_gazebo motor_gazebo_eeros.launch'
 
