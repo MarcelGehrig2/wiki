fb0029099b931dad2667f6d93de20d23023813bc
MarcelGehrig <marcel.gehrig@ntb.ch>
2018-04-13 16:03:40 +0200

    commit

---
+++ linux/scripts/inUse/myBashrcChair.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# sourced by ~/.bashrc.sh
+
+
+
+alias sshmainpcp='ssh mainpcp@146.136.39.214 -p 9022'
+alias sshmasterpc='ssh chair@98.116.5.90 -p 9006'
+alias sshmainpc1='ssh mainpcp@98.116.5.90 -p 9016'
+alias sshmainpc2='ssh mainpcp@98.116.5.90 -p 9026'
+alias sshmainpc3='ssh mainpcp@98.116.5.90 -p 9036'
+alias sshmainpc4='ssh mainpcp@98.116.5.90 -p 9046'
+alias sshmainpc5='ssh mainpcp@98.116.5.90 -p 9056'
+alias sshmainpc6='ssh mainpcp@98.116.5.90 -p 9066'
+alias sshmainpc7='ssh mainpcp@98.116.5.90 -p 9076'
+alias sshmainpc8='ssh mainpcp@98.116.5.90 -p 9086'
+alias sshmainpc9='ssh mainpcp@98.116.5.90 -p 9096'
+
+alias scpmainpcp='scp -P 9022 mainpcp@146.136.39.214:'
+alias scpmasterpc='scp -P 9006 chair@98.116.5.90:'
+alias scpmainpc1='scp -P 9016 mainpcp@98.116.5.90:'
+alias scpmainpc2='scp -P 9026 mainpcp@98.116.5.90:'
+alias scpmainpc3='scp -P 9036 mainpcp@98.116.5.90:'
+alias scpmainpc4='scp -P 9046 mainpcp@98.116.5.90:'
+alias scpmainpc5='scp -P 9056 mainpcp@98.116.5.90:'
+alias scpmainpc6='scp -P 9066 mainpcp@98.116.5.90:'
+alias scpmainpc7='scp -P 9076 mainpcp@98.116.5.90:'
+alias scpmainpc8='scp -P 9086 mainpcp@98.116.5.90:'
+alias scpmainpc9='scp -P 9096 mainpcp@98.116.5.90:'
+
+
