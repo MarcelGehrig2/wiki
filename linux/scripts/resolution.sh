7daf1de22ecec9b46a2f0c38d187294c1f9b199b
MarcelGehrig <marcel.gehrig@noser.com>
2019-09-05 10:25:41 +0200

    commit

--- linux/scripts/resolution.sh
+++ linux/scripts/resolution.sh
@@ -1,11 +1,9 @@
-#https://askubuntu.com/questions/712564/why-cant-i-see-169-aspect-ratios
+#!/bin/bash
 
-
-sudo cvt 1920 1080 60
 
 sudo xrandr --newmode "1920x1080_60.00"  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
 
-xrandr
-
 sudo xrandr --addmode Virtual1 1920x1080_60.00
+
+sudo xrandr --output Virtual1 --mode "1920x1080_60.00"
 
